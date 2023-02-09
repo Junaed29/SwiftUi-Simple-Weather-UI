4703fe77acc783e45ddc2be01b57ac0e894df95a
Junaed <junaed1995@gmail.com>
2023-02-09 12:16:11 +0600

    Initial Commit

---
+++ SwiftUi-Weather/SwiftUi_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_WeatherApp.swift
+//  SwiftUi-Weather
+//
+//  Created by Junaed Muhammad Chowdhury on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
